70b58c241f06bb6b616a0d39f79fbb6b2f8edf96
jakkujakku <jakkujakku98@gmail.com>
2023-12-14 15:26:21 +0900

    [FEAT]: MVVM 폴더 구조 생성

---
+++ Pyeon-Duck/Models/ExpirationDate.swift
@@ -0,0 +1,8 @@
+//
+//  ExpirationDate.swift
+//  Pyeon-Duck
+//
+//  Created by 준우의 MacBook 16 on 12/14/23.
+//
+
+import Foundation
---
+++ Pyeon-Duck/Views/SalaryVC.swift
@@ -0,0 +1,29 @@
+//
+//  SalaryVC.swift
+//  Pyeon-Duck
+//
+//  Created by 준우의 MacBook 16 on 12/14/23.
+//
+
+import UIKit
+
+class SalaryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
